717c464c4b988143f9f0b7157d8318ea1dc8bf7b
Sejal Koli <116626091+sejalkoli@users.noreply.github.com>
2024-04-12 18:50:22 +0530

    Add files via upload

---
+++ weatherapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+def home(request):
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'delhi'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=110ba5d4c7a06630af3dd0542c2f277a'
+    PARAMS = {'units':'metric'}
+
+    API_KEY =  'AIzaSyCdws7X3L2e8qilcgYGo-SlD2Sf7AnWT7E'
+
+    SEARCH_ENGINE_ID = 'f6ebc50db3fec4afa'
+     
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+    
+
+    try:
+          
+          data = requests.get(url,params=PARAMS).json()
+          description = data['weather'][0]['description']
+          icon = data['weather'][0]['icon']
+          temp = data['main']['temp']
+          day = datetime.date.today()
+
+          return render(request,'index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city , 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+          exception_occurred = True
+          messages.error(request,'Entered data is not available to API')   
+
+          day = datetime.date.today()
+
+          return render(request,'index.html' ,{'description':'clear sky', 'icon':'01d'  ,'temp':25 , 'day':day , 'city':'indore' , 'exception_occurred':exception_occurred } )
+               
+    
+    
